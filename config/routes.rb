bbeb64a69ee7ec7e41bd0737bb472e8cb0ecd1be
André Nogueira <andreedsnogueira@gmail.com>
2018-12-18 18:47:53 +0000

    API Controller and subdomain implemented

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :tasks
   devise_for :administrators
   devise_for :employees
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+    # list of resources
+  end
 end
