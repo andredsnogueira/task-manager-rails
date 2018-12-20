338a4bf7b4d0cda575bc425d4e1af248eb0242e9
André Nogueira <andreedsnogueira@gmail.com>
2018-12-20 04:11:41 +0000

    quick fix

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -26,7 +26,7 @@ class TasksController < ApplicationController
   # POST /tasks.json
   def create
     @task = Task.new(task_params)
-    @employee = Employee.find(1)
+    @employee = Employee.find(params[:employee_id])
     respond_to do |format|
       if @task.save
         @employee.tasks << @task
